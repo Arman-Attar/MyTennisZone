4d30dafa3d0e1c92c03ef3edf335170560949f15
Arman Zadehattar <arman.zattar@gmail.com>
2022-04-28 16:48:39 -0400

    Initial Commit

---
+++ TennisTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TennisTracker
+//
+//  Created by Arman Zadeh-Attar on 2022-04-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TennisTracker/TennisTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisTrackerApp.swift
+//  TennisTracker
+//
+//  Created by Arman Zadeh-Attar on 2022-04-28.
+//
+
+import SwiftUI
+
+@main
+struct TennisTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
